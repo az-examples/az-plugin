405d2024b7230663ef750f8b547ce704392c361a
Nathan Poirier <nathan@poirier.io>
2025-01-17 07:19:38 +0100

    Add az() shortcuts for entities

--- bukkit/api/src/main/java/fr/nathan818/azplugin/bukkit/AZBukkitShortcuts.java
+++ bukkit/api/src/main/java/fr/nathan818/azplugin/bukkit/AZBukkitShortcuts.java
@@ -1,7 +1,9 @@
 package fr.nathan818.azplugin.bukkit;
 
+import fr.nathan818.azplugin.bukkit.entity.AZEntity;
 import fr.nathan818.azplugin.bukkit.entity.AZPlayer;
 import lombok.experimental.UtilityClass;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
@@ -17,5 +19,15 @@ public class AZBukkitShortcuts {
     @Contract(value = "null -> null")
     public static @Nullable AZPlayer az(@Nullable Player player) {
         return AZBukkit.api().getClient(player);
+    }
+
+    @Contract(value = "null -> null")
+    public static @Nullable AZEntity az(@Nullable Entity entity) {
+        return AZBukkit.api().getEntity(entity);
+    }
+
+    @Contract(value = "null -> null")
+    public static @Nullable AZEntity azIfPresent(@Nullable Entity entity) {
+        return AZBukkit.api().getEntityIfPresent(entity);
     }
 }
